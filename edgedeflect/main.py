e689209017c81cd191a743ba9370976cb1bff557
Blazzycrafter <kai.knechtl@gmail.com>
2023-05-07 11:03:37 +0200

    Base 2

--- edgedeflect/main.py
+++ edgedeflect/main.py
@@ -1,20 +1,49 @@
 import argparse
 
+
+import os
+import configparser
+
+
 parser = argparse.ArgumentParser(description='D')
-parser.add_argument('--windows-start', help='H1')
+parser.add_argument('--windowsstart', help='H1')
+
+
+
+
 args = parser.parse_args()
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 def init():
     ### here we init the script like creating folders and changing run locations...
     ### and variable... so on...
+    installDir = ""
+    if installDir != "" :
+        os.chdir(installDir)
+
+
+
+
+
+    # Load Ini-file
+    ConfigDeployMethod = config.get('Settings', 'DeployMethod')
+
+    print(ConfigDeployMethod)
+
+
     pass
 
 
-if args.option1:
-    # run code on pc start
-    pass
 
-if not args.option1:
-    # here we can set settings...
-    pass
+if __name__ == '__main__':
+
+    init()
+    if args.windowsstart:
+        # run code on pc start
+        pass
+
+    if not args.windowsstart:
+        # here we can set settings...
+        pass
